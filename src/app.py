c99c7065591e087a17750fe194911a84cc0e230b
Brad Cooley <bcooley@michiganlabs.com>
2023-09-03 00:09:34 -0400

    Fixed spacing

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 from recording_thread import RecordingThread
 
 recording_thread: RecordingThread or None = None
-save_directory = '/home/phone/Documents/wedding_voicemail_audio_files'
+save_path = '/home/phone/Documents/wedding_voicemail_audio_files'
 logging.basicConfig(level=logging.INFO)
 
 
@@ -15,7 +15,7 @@ def phone_picked_up():
 	logging.info('Receiver picked up')
 	global recording_thread
 
-	recording_thread = RecordingThread(save_dir=save_directory)
+	recording_thread = RecordingThread(save_dir=save_path)
 	recording_thread.start()
 
 
@@ -47,10 +47,10 @@ def listen_for_hook_state_change():
 
 
 if __name__ == "__main__":
-	save_path = sys.argv[1] if len(sys.argv) > 1 else save_dir
+	save_path = sys.argv[1] if len(sys.argv) > 1 else save_path
 	logging.info('Using path {}'.format(save_path))
 	if not os.path.exists(save_path):
-        os.mkdir(save_path)
-        logging.info('Given path doesn\'t exist. Path created')
-        
+		os.mkdir(save_path)
+		logging.info('Given path doesn\'t exist. Path created')
+		
 	listen_for_hook_state_change()
