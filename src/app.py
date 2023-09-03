2fe4369731900f4a00918cd711828b80e5a05c20
Brad Cooley <brad@cooleyweb.org>
2023-09-03 00:08:37 -0400

    Path issues

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import time
 import sys
 
@@ -6,7 +7,7 @@ import RPi.GPIO as GPIO
 from recording_thread import RecordingThread
 
 recording_thread: RecordingThread or None = None
-save_dir = '/home/phone/Documents/wedding_voicemail_audio_files'
+save_directory = '/home/phone/Documents/wedding_voicemail_audio_files'
 logging.basicConfig(level=logging.INFO)
 
 
@@ -14,7 +15,7 @@ def phone_picked_up():
 	logging.info('Receiver picked up')
 	global recording_thread
 
-	recording_thread = RecordingThread(save_dir=save_dir)
+	recording_thread = RecordingThread(save_dir=save_directory)
 	recording_thread.start()
 
 
@@ -46,6 +47,10 @@ def listen_for_hook_state_change():
 
 
 if __name__ == "__main__":
-	save_dir = sys.argv[1] if len(sys.argv) > 1 else save_dir
-	logging.info(f"Using save directory: {save_dir}")
+	save_path = sys.argv[1] if len(sys.argv) > 1 else save_dir
+	logging.info('Using path {}'.format(save_path))
+	if not os.path.exists(save_path):
+        os.mkdir(save_path)
+        logging.info('Given path doesn\'t exist. Path created')
+        
 	listen_for_hook_state_change()
