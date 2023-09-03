579ae2ebf5643ec0dbcb80845db2e2e33645cf12
Brad Cooley <bcooley@michiganlabs.com>
2023-09-02 21:12:01 -0400

    Added audio files

--- recording_thread.py
+++ recording_thread.py
@@ -12,16 +12,16 @@ import soundfile as sf
 import queue
 import sys
 
+
 class RecordingThread(Thread):
 
-    def __init__(self, channels=1, frame_rate=44100, chunk=4096, device_index=1):
+    def __init__(self, subtype='PCM_16', channels=1, frame_rate=44100, chunk=4096, device_index=1):
         super().__init__()
+        self.__SUBTYPE = subtype
         self.__CHANNELS = channels
         self.__FRAME_RATE = frame_rate
         self.__CHUNK = chunk
         self.__DEVICE_INDEX = device_index
-
-        self.__SUBTYPE = 'PCM_16'
 
         self.__id = uuid.uuid1()
         __root_dir = os.path.dirname(__file__)
@@ -30,10 +30,8 @@ class RecordingThread(Thread):
         self.__recording = False
 
         pygame.mixer.init()
-        self.__VOICE_MESSAGE = pygame.mixer.Sound('/home/phone/Documents/wedding-phone/mysound.wav')
-
-        self.__STREAM = None
-        self.__frames = []
+        self.__VOICE_MESSAGE = pygame.mixer.Sound('/home/phone/Documents/wedding-phone/VoiceMessage.wav')
+        self.__MESSAGE_TONE = pygame.mixer.Sound('/home/phone/Documents/wedding-phone/beep.wav')
         
         self.__stop_event = Event()
 
@@ -50,22 +48,14 @@ class RecordingThread(Thread):
         while pygame.mixer.get_busy():
             pass
 
+    def __play_message_tone(self):
+        logging.info("Playing beep")
+        pygame.time.wait(10)
+        self.__MESSAGE_TONE.play()
+        while pygame.mixer.get_busy():
+            pass
+
     def run(self):
-        async def inner_run():
-            # Create and gather all the asynchronous tasks
-            tasks = [
-                #self.__create_file(),
-                self.__play_voice_message()
-                #self.__setup_stream()
-            ]
-
-            # Wait for all the tasks to complete
-            await asyncio.gather(*tasks)
-
-            # Now, you can play the beep sound or perform any other action
-            logging.info("All tasks completed, playing beep")
-
-        asyncio.run(inner_run())
 
         def callback(indata, frames, time, status):
             """This is called (from a separate thread) for each audio block."""
@@ -73,17 +63,41 @@ class RecordingThread(Thread):
                 print(status, file=sys.stderr)
             q.put(indata.copy())
 
+        async def inner_run():
+            # Create and gather all the asynchronous tasks
+            tasks = [
+                self.__play_voice_message()
+            ]
+
+            # Wait for all the tasks to complete
+            await asyncio.gather(*tasks)
+
+            # Now, you can play the beep sound or perform any other action
+            logging.info("All tasks completed, safe to play beep")
+            self.__play_message_tone()
+
+        asyncio.run(inner_run())
 
         q = queue.Queue()
 
         # Make sure the file is opened before recording anything:
-        with sf.SoundFile(self.__filepath, mode='x', samplerate=self.__FRAME_RATE,
-                          channels=self.__CHANNELS, subtype=self.__SUBTYPE) as file:
+        with sf.SoundFile(file=self.__filepath,
+                          mode='x',
+                          samplerate=self.__FRAME_RATE,
+                          channels=self.__CHANNELS,
+                          subtype=self.__SUBTYPE
+                          ) as file:
+
             logging.debug('File {} successfully created'.format(file.name))
-            with sd.InputStream(samplerate=self.__FRAME_RATE, device=self.__DEVICE_INDEX,
-                                channels=self.__CHANNELS, callback=callback):
+
+            with sd.InputStream(samplerate=self.__FRAME_RATE,
+                                device=self.__DEVICE_INDEX,
+                                channels=self.__CHANNELS,
+                                callback=callback):
+
                 logging.info('Recording...')
                 self.__recording = True
+
                 while self.__recording:
                     if self.__stop_event.is_set():
                         break
