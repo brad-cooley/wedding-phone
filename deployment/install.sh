0047705260e3286f5d810b1bc6ebe5797c6a12fb
Brad Cooley <brad@cooleyweb.org>
2023-09-08 02:10:14 -0400

    Test file

--- deployment/install.sh
+++ deployment/install.sh
@@ -8,9 +8,6 @@ sudo apt-get install -y \
         libffi-dev \
         libssl-dev \
         portaudio19-dev
-python3 -m pip install virtualenv
-python3 -m virtualenv -p python3 env
-. env/bin/activate
 
 pip install --upgrade pip
 
