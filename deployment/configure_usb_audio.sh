1eec88e1a3e43716e3f60fab66987181351058e4
Brad Cooley <bcooley@michiganlabs.com>
2023-09-08 02:06:01 -0400

    defaults updated

--- deployment/configure_usb_audio.sh
+++ deployment/configure_usb_audio.sh
@@ -8,6 +8,6 @@ aplay -l
 CONFIG_PATH=/usr/share/alsa/alsa.conf
 sudo cp $CONFIG_PATH $CONFIG_PATH.bak
 
-echo "Setting output defaults for pi zero"
-sudo sed -i 's/defaults.ctl.card 0/defaults.ctl.card 1/g' $CONFIG_PATH
-sudo sed -i 's/defaults.pcm.card 0/defaults.pcm.card 1/g' $CONFIG_PATH
+echo "Setting output defaults for Pi"
+sudo sed -i 's/defaults.ctl.card 0/defaults.ctl.card 2/g' $CONFIG_PATH
+sudo sed -i 's/defaults.pcm.card 0/defaults.pcm.card 2/g' $CONFIG_PATH
